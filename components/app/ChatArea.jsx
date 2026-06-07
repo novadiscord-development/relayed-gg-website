8b6a39bd3eab5d80042d825e1fc52b9577315aa7
novadiscord-development <dylanlayton098@gmail.com>
2026-06-07 16:35:16 +0100

    Update ChatArea.jsx

--- components/app/ChatArea.jsx
+++ components/app/ChatArea.jsx
@@ -506,8 +506,6 @@ export default function ChatArea() {
 
   if (names.length === 1) {
     text = `${names[0]} is typing`;
-  } else if (names.length === 2) {
-    text = `${names[0]} and ${names[1]} are typing`;
   } else {
     text = "Several people are typing";
   }
