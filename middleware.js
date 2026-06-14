668ddade79af12d3ef06038a8721a899c88e9fe7
novadiscord-development <dylanlayton098@gmail.com>
2026-06-14 17:40:55 +0100

    updated MIddleware.js

--- middleware.js
+++ middleware.js
@@ -1,10 +1,6 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-function isAuthPage(pathname) {
-  return pathname === "/login" || pathname === "/register";
-}
-
 export async function middleware(req) {
   const { pathname, search } = req.nextUrl;
 
@@ -12,10 +8,6 @@ export async function middleware(req) {
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
-
-  if (isAuthPage(pathname) && token) {
-    return NextResponse.redirect(new URL("/app", req.url));
-  }
 
   if (pathname.startsWith("/app") && !token) {
     const loginUrl = new URL("/login", req.url);
@@ -27,5 +19,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/app/:path*", "/login", "/register"],
+  matcher: ["/app/:path*"],
 };
